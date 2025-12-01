7f7c79fc34dc6966d4e557dc48e03d2bca59e1ce
JakobStorm7913 <156937915+JakobStorm7913@users.noreply.github.com>
2025-12-01 13:26:51 +0100

    Added gamedata and manager + scripts for main screen

---
+++ Assets/Scripts/Managers/GameData.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+ public static GameData Instance { get; private set; }
+
+    [Header("Score Data")]
+    public float RoundScore { get; set; }
+    public float LastHighscore { get; set; }
+    public float Highscore { get; set; }
+    public bool NewHighscoreWasSest { get; set; }
+
+    [Header("Coin Data")]
+    public int RoundCoins { get; set; }
+    public int LastTotalCoins { get; set; }
+    public int NewTotalCoins { get; set; }
+
+    [Header("Other")]
+    public bool GameRunning { get; set; } 
+    public string Difficulty { get; set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            // Initialize default values
+            LastHighscore = 0;
+            Highscore = 0;
+            LastTotalCoins = 0;
+            NewTotalCoins = 0;
+            Difficulty = "Easy";
+            GameRunning = false;
+            NewHighscoreWasSest = false;
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
