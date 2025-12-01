88cde43cd09737df65b52b09338bfe79180c2cc3
jonathanhj123 <jona246@gmail.com>
2025-12-01 09:21:30 +0100

    xD

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public bool IsAlive = true;
+    public Rigidbody2D rb2D;
+    public float Movementspeed = 10;
+    private Animator anim;
+
+   // public LogicManager logic;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+  //      logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManager>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D bird)
+    {
+        die();
+    }
+
+    public void die()
+    {
+        IsAlive = false;
+        Destroy(gameObject);
+        Debug.Log("Bird is dead");
+//        logic.gameOver();
+    }
+
+    public void OnJump()
+    {
+        if (IsAlive)
+        {
+            anim.Play("Jumping", 0, 0.25f);
+            rb2D.linearVelocity = Vector2.up * Movementspeed;
+        }
+    }
+}
---
+++ Assets/Scripts/PipeMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PipeMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float movementspeed = 10;
+    public float deadZone = -12;
+    private BirdScript birdscript;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void awake()
+    {
+    //    bird = GameObject.FindWithTag("Bird");
+    }
+    void Start()
+    {
+//        birdscript = bird.GetComponent<BirdScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindWithTag("Bird");
+        birdscript = player.GetComponent<BirdScript>();
+        if (birdscript.IsAlive)
+        {
+            transform.position = transform.position + (Vector3.left * movementspeed) * Time.deltaTime;
+        }
+        if (transform.position.x < deadZone)
+        {
+            DestroyObject();
+        }
+
+
+    }
+
+    void DestroyObject()
+    {
+        Debug.Log("Pipe Destroyed");
+        Destroy(gameObject);
+    }
+
+}
