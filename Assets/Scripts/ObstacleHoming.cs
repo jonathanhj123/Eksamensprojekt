a5a80ede89b3de24306d2cebed441a002e3d7669
jonathanhj123 <57926140+jonathanhj123@users.noreply.github.com>
2025-12-02 09:50:48 +0100

    Revert "j"
    This reverts commit 002e0620df5b6d4cf3c2be4fcf8072b43fd822d5.

--- Assets/Scripts/ObstacleHoming.cs
+++ Assets/Scripts/ObstacleHoming.cs
@@ -5,7 +5,7 @@ public class ObstacleHoming : MonoBehaviour
 {
     //Variable
 
-    //Sætter objektets bevægelseshastighed til 3 enheder per sekund, men kan ændres i inspector.
+    //Sætter objektets bevægelseshastighed til 3 enheder per sekund.
     public float moveSpeed = 3f;
     
     //Definerer en Transform-variabel kaldet targetTransform, som vil blive brugt til at angive målets position.
@@ -24,8 +24,11 @@ public class ObstacleHoming : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Beregner retningen mod målet ved at trække objektets position fra målpositionen.
+        Vector2 direction = targetTransform.position - transform.position;
         
-        transform.position = Vector3.MoveTowards(transform.position, targetTransform.position, moveSpeed * Time.deltaTime);
+        direction.Normalize();
 
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
 }
